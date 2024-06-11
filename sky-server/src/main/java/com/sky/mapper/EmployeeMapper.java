45e21c1441b8fd490dea3be299dd4f6ce29468c3
ouyangzhiyong <ouyangzhiyong0415@qq.com>
2024-06-11 20:17:14 +0800

    add the common keys autofill module

--- sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
+++ sky-server/src/main/java/com/sky/mapper/EmployeeMapper.java
@@ -1,8 +1,10 @@
 package com.sky.mapper;
 
 import com.github.pagehelper.Page;
+import com.sky.annotation.AutoFill;
 import com.sky.dto.EmployeePageQueryDTO;
 import com.sky.entity.Employee;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
@@ -20,6 +22,7 @@ public interface EmployeeMapper {
     /*
     * 插入员工记录
     * */
+    @AutoFill(value = OperationType.INSERT)
     @Insert(" insert into employee (name, username, password, phone, sex, id_number, create_time, update_time, create_user, update_user, status) values (#{name},#{username},#{password},#{phone},#{sex},#{idNumber},#{createTime},#{updateTime},#{createUser},#{updateUser},#{status}) ")
     void insert(Employee employee);
     /*
@@ -31,6 +34,7 @@ public interface EmployeeMapper {
     /*
     * 员工账号状态更新,根据主键动态修改属性，因此还是写在xml里
     * */
+    @AutoFill(value = OperationType.UPDATE)
     void update(Employee employee);
 
     @Select("select * from employee where id = #{id}")
